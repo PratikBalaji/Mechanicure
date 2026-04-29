3d0c6ad05bebf71c3515d93181edb8f947eb43cd
PratikBalaji <balajipratik8@gmail.com>
2026-04-29 18:52:29 -0400

    Fix TypeScript MONGODB_URI string inference

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -28,7 +28,7 @@ export default async function dbConnect(): Promise<typeof mongoose> {
   }
 
   if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI, {
+    cached.promise = mongoose.connect(MONGODB_URI as string, {
       bufferCommands: false,
     });
   }
